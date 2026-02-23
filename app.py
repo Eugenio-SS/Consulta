cd4ec97c63715224353d55fbd70f1ed52ec3e270
Eugenio-SS <serviciosocial1602@gmail.com>
2026-02-23 13:36:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,9 @@ with st.sidebar:
     st.markdown("---")
     with st.expander("Información"):
         st.write("Versión: 1.0")
-        st.write("INBAL")
+        st.write("      INBAL")
+        st.write("")
+        st.write("")
         st.write("Firma técnica: **Eduardo Eugenio Badillo Melo**")
 
 # 4. LÓGICA DE CONSULTA
@@ -89,4 +91,5 @@ else:
     st.info(" El sistema no tiene datos cargados. El administrador debe subir un Excel.")
 
 st.markdown('<div class="footer">INBAL | EEBM</div>', unsafe_allow_html=True)
+
 
